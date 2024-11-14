30af5469e21e4508bb7bdc6e70ab285858f63ff9
Gustavo <tarrs@protonmail.com>
2024-11-14 19:20:26 -0300

    refactor NotFound component

--- src/views/NotFound.tsx
+++ src/views/NotFound.tsx
@@ -45,11 +45,11 @@ const NotFound = ({ mode }: { mode: SystemMode }) => {
           <Typography className='font-medium text-8xl' color='text.primary'>
             404
           </Typography>
-          <Typography variant='h4'>Page Not Found ⚠️</Typography>
+          <Typography variant='h4'>Página não encontrada</Typography>
           <Typography>we couldn&#39;t find the page you are looking for.</Typography>
         </div>
         <Button href='/' component={Link} variant='contained'>
-          Back To Home
+          Voltar para o início
         </Button>
         <img
           alt='error-404-illustration'
