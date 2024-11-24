8d54a3c81bc5259464eb81a1c75156799d74f696
Gustavo <tarrs@protonmail.com>
2024-11-23 23:34:58 -0300

    chore: redirect to Login page when there's no user in localStorage

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,5 +1,8 @@
 'use client'
 
+// Next imports
+import { useRouter } from 'next/navigation'
+
 // React imports
 import React, { useEffect, useRef, useState } from 'react'
 
@@ -13,15 +16,25 @@ import SearchComponent from './Search'
 import { Skeleton } from '@mui/material'
 
 const Map = () => {
+  const router = useRouter()
+
+  // Refs
   const mapContainer = useRef<HTMLDivElement>(null)
   const map = useRef<mapboxgl.Map | null>(null)
+  // States
   const [center, setCenter] = useState<LngLatLike>([-49.2730, -25.4277])
   const [zoom, setZoom] = useState(10.12)
   const [isMapLoaded, setIsMapLoaded] = useState(false)
 
   useEffect(() => {
-    mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN!
+    const user = localStorage.getItem('user')
+    if (!user) {
+      router.push('/login')
+    }
+  }, [router])
 
+  useEffect(() => {
+    mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN!
     if (!mapContainer.current) return
 
     map.current = new mapboxgl.Map({
